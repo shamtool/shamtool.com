92f93b196cd29bef941a870c4aa23041dbc02058
Crollie Rollz <crollierollz@gmail.com>
2022-02-15 03:56:05 +0800

    remove card margin

--- src/lib/components/MapCardContainer.svelte
+++ src/lib/components/MapCardContainer.svelte
@@ -1,5 +1,5 @@
 <!-- Style adapted from https://tailwindcomponents.com/component/full-responsive-video-cards (by amirrahman132132) -->
 
-<div class="mx-auto w-11/12 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+<div class="mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
     <slot />
 </div>
